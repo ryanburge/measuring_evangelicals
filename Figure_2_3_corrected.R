2a9c754f6d6cb2b27548aaf722f19015ee9130b1
Ryan Burge <ryanburge@gmail.com>
2018-08-11 12:09:30 -0500

    dramatically streamlined the analysis.

--- Figure_2_3_corrected.R
+++ Figure_2_3_corrected.R
@@ -1,113 +1,128 @@
+## This is to create consistent variable names across all waves of the CCES
 
+cces08 <- cces08 %>% 
+  rename(protestant = V219, bagain = V215, weight = V201, pid7 = CC307a) %>% 
+  mutate(year = 2008) 
 
-c08 <- cces08 %>% as_survey_design(weights = V201)
-c10 <- cces10 %>% as_survey_design(weights = V101)
-c12 <- cces12 %>% as_survey_design(weights = weight_vv)
-c14 <- cces14 %>% as_survey_design(weights = weight)
-c16 <- cces16 %>% as_survey_design(weights = commonweight_vv)
+cces10 <- cces10 %>% 
+  rename(protestant = V219, bagain = V215, weight = V101, pid7 = V212d) %>% 
+  mutate(year = 2010)
 
-p1 <- c08 %>% 
-  filter(baprot ==1) %>%  filter(V211 !=2) %>% 
-  summarise(mean = survey_mean(CC307a, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2008")
+cces12 <- cces12 %>% 
+  rename(protestant = religpew, bagain = pew_bornagain, weight = weight_vv) %>% 
+  mutate(year = 2012)
 
-p2 <- c08 %>% 
+cces14 <- cces14 %>% 
+  rename(protestant = religpew, bagain = pew_bornagain, weight = weight) %>% 
+  mutate(year = 2014)
+
+cces16 <- cces16 %>% 
+  rename(protestant = religpew, bagain = pew_bornagain, weight = commonweight_vv) %>% 
+  mutate(year = 2016)
+
+### This is code the attendance variable so that higher values = more attendance
+
+cces08$att <- 7 - as.numeric(cces08$V217)
+cces10$att <- 7 - as.numeric(cces10$V217)
+cces12$att <- 7 - as.numeric(cces12$pew_churatd)
+cces14$att <- 7 - as.numeric(cces14$pew_churatd)
+cces16$att <- 7 - as.numeric(cces16$pew_churatd)
+
+## Now creating smaller versions of our dataset with only the variables are essential to analysis
+
+c08 <- cces08 %>% 
+  select(year, protestant, evangelical, bagain, race, weight, pid7, att)
+
+c10 <- cces10 %>% 
+  select(year, protestant, evangelical, bagain, race, weight, pid7, att)
+
+c12 <- cces12 %>% 
+  select(year, protestant, evangelical, bagain, race, weight, pid7, att)
+
+c14 <- cces14 %>% 
+  select(year, protestant, evangelical, bagain, race, weight, pid7, att)
+
+c16 <- cces16 %>% 
+  select(year, protestant, evangelical, bagain, race, weight, pid7, att)
+
+## Bind them all together into one big dataframe called all_cc
+
+all_cc <- bind_rows(c08, c10, c12, c14, c16) %>% as.tibble()
+
+## Party ID for Evangelicals 
+
+c1 <- all_cc %>% 
   filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(CC307a, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2008")
+  group_by(year) %>%  
+  filter(pid7 <= 7) %>% ## exclude those who said other party id 
+  summarise(mean = weighted.mean(pid7, w = weight, na.rm = TRUE),
+            sd = sd(pid7, na.rm = TRUE), 
+            n = n()) %>% 
+  mutate(se = sd/sqrt(n),
+         lower = mean - qt(1 - (0.05 /2),  n -1) * se,
+         upper = mean + qt(1 - (0.05 /2),  n -1) * se) %>% 
+  mutate(type = "Affiliation")
 
+## Party ID for Self IDs
 
-p3 <- c10 %>% 
-  filter(baprot ==1) %>% filter(V211 !=2) %>% 
-  summarise(mean = survey_mean(V212d, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2010")
+c2 <- all_cc %>% 
+  filter(protestant ==1 & bagain ==1 & race != 2) %>% 
+  group_by(year) %>%  
+  filter(pid7 <= 7) %>% 
+  summarise(mean = weighted.mean(pid7, w = weight, na.rm = TRUE),
+            sd = sd(pid7, na.rm = TRUE), 
+            n = n()) %>% 
+  mutate(se = sd/sqrt(n),
+         lower = mean - qt(1 - (0.05 /2),  n -1) * se,
+         upper = mean + qt(1 - (0.05 /2),  n -1) * se) %>% 
+  mutate(type = "Self ID")
 
-p4 <- c10 %>% 
-  filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(V212d, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2010")
+## Bind together and add a survey variable 
+cc <- bind_rows(c1, c2)
+cc$survey <- paste("CCES", cc$year, sep = " ")
 
-
-p5 <- c12 %>% 
-  filter(baprot ==1) %>% 
-  filter(race !=2) %>%
-  summarise(mean = survey_mean(pid7, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2012")
-
-p6 <- c12 %>% 
-  filter(evangelical ==1) %>%
-  summarise(mean = survey_mean(pid7, vartype = "ci", level = .84, na.rm =TRUE))%>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2012")
-
-
-p7 <- c14 %>% 
-  filter(baprot ==1) %>% 
-  filter(race !=2) %>%
-  summarise(mean = survey_mean(pid7, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2014")
-
-p8 <- c14 %>% 
-  filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(pid7, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2014")
-
-
-p9 <- c16 %>% 
-  filter(baprot ==1) %>% 
-  filter(race !=2) %>%
-  summarise(mean = survey_mean(pid7, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2016")
-
-p10 <- c16 %>% 
-  filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(pid7, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2016")
-
-pp <- bind_rows(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
-
+## Restrict our Analysis to 2008 to 2016
 gss <- gss %>% filter(year > 2007)
 
-gss <- gss %>% 
-    mutate(pid = partyid + 1) # The GSS is coded 0 to 6. The CCES is 1 to 7. This is how I correct that. 
-
-gs <- gss %>% as_survey_design(weights = wtssall)
-
-g1 <- gs %>% 
+g1 <- gss %>% 
   filter(evangelical ==1) %>% 
-  group_by(year) %>% 
-  summarise(mean = survey_mean(pid, vartype = "ci", level = .84, na.rm = TRUE)) %>% 
-  mutate(type = c("Affiliation"))
+  group_by(year) %>%  
+  mutate(pid = partyid + 1) %>% ## must add to pid for GSS because it's coded 0 to 6, while CCES is 1 to 7
+  filter(pid <= 7) %>% ## exclude those who said other party id 
+  summarise(mean = weighted.mean(pid, w = wtss, na.rm = TRUE),
+            sd = sd(pid, na.rm = TRUE), 
+            n = n()) %>% 
+  mutate(se = sd/sqrt(n),
+         lower = mean - qt(1 - (0.05 /2),  n -1) * se,
+         upper = mean + qt(1 - (0.05 /2),  n -1) * se) %>% 
+  mutate(type = "Affiliation")
 
-g2 <- gs %>% 
-  filter(reborn ==1 & relig ==1 & race != 2) %>% 
-  group_by(year) %>% 
-  summarise(mean = survey_mean(pid, vartype = "ci", level = .84, na.rm = TRUE)) %>% 
-  mutate(type = c("Self ID"))
+g2 <- gss %>% 
+  filter(baprot2 ==1) %>% 
+  group_by(year) %>%  
+  mutate(pid = partyid + 1) %>% 
+  filter(pid <= 7) %>% 
+  summarise(mean = weighted.mean(pid, w = wtss, na.rm = TRUE),
+            sd = sd(pid, na.rm = TRUE), 
+            n = n()) %>% 
+  mutate(se = sd/sqrt(n),
+         lower = mean - qt(1 - (0.05 /2),  n -1) * se,
+         upper = mean + qt(1 - (0.05 /2),  n -1) * se)  %>% 
+  mutate(type = "Self ID")
 
 gg <- bind_rows(g1, g2)
 
 gg$survey <- paste("GSS", gg$year, sep = " ")
 
-total <- bind_rows(pp, gg) %>% select(-year)
+## Add both results into one dataset
+total <- bind_rows(cc, gg)
 
-# write.csv(total, "pid_table.csv")
-
-
+## Graph
 ggplot(total, aes(x = mean, y = survey))  +
   geom_point(shape=21, size =4, aes(fill = factor(type))) +  theme(legend.title=element_blank()) +
-  geom_errorbarh(aes(xmin = mean_low, xmax=mean_upp), height=.075, size = 1, show.legend = FALSE) +
+  geom_errorbarh(aes(xmin = lower, xmax=upper), height=.075, size = 1, show.legend = FALSE) +
   theme(legend.position = "bottom")  + scale_fill_brewer(palette = "Set2") + 
-  labs(x ="Year And Survey", y ="Self Identified Party ID", caption = "84% Confidence Intervals Displayed") +
+  labs(y ="Year And Survey", x ="Self Identified Party ID", caption = "95% Confidence Intervals Displayed") +
   ggtitle("Mean Partisanship of Each Measurement")+ 
   theme(plot.title = element_text(hjust = 0.5))+
   theme(text=element_text(size=24, family="KerkisSans"))  +
@@ -119,161 +134,72 @@ ggsave(file="figure_2.png", type = "cairo-png", width = 15, height = 10)
 
 ## Making Attendance Graph ####
 
-
-cces08$attend <- 7 - as.numeric(cces08$V217)
-# cces08$attend <- Recode(cces08$attend, "0= 'Do not Know';
-#                         1= 'Never';
-#                         2= 'Seldom';
-#                         3= 'Yearly';
-#                         4= 'Monthly';
-#                         5= 'Weekly';
-#                         6= 'More than Weekly'")
-
-
-cces10$attend <- 7 - as.numeric(cces10$V217)
-# cces10$attend <- Recode(cces10$attend, "0= 'Do not Know';
-#                         1= 'Never';
-#                         2= 'Seldom';
-#                         3= 'Yearly';
-#                         4= 'Monthly';
-#                         5= 'Weekly';
-#                         6= 'More than Weekly'")
-
-cces12$attend <- 7 - as.numeric(cces12$pew_churatd)
-# cces12$attend <- Recode(cces12$attend, "0= 'Do not Know';
-#                         1= 'Never';
-#                         2= 'Seldom';
-#                         3= 'Yearly';
-#                         4= 'Monthly';
-#                         5= 'Weekly';
-#                         6= 'More than Weekly'")
-
-cces14$attend <- 7 - as.numeric(cces14$pew_churatd)
-# cces14$attend <- Recode(cces14$attend, "0= 'Do not Know';
-#                         1= 'Never';
-#                         2= 'Seldom';
-#                         3= 'Yearly';
-#                         4= 'Monthly';
-#                         5= 'Weekly';
-#                         6= 'More than Weekly'")
-
-cces16$attend <- 7 - as.numeric(cces16$pew_churatd)
-# cces16$attend <- Recode(cces16$attend, "0= 'Do not Know';
-#                         1= 'Never';
-#                         2= 'Seldom';
-#                         3= 'Yearly';
-#                         4= 'Monthly';
-#                         5= 'Weekly';
-#                         6= 'More than Weekly'")
-
-
-c08 <- cces08 %>% as_survey_design(weights = V201)
-c10 <- cces10 %>% as_survey_design(weights = V101)
-c12 <- cces12 %>% as_survey_design(weights = weight_vv)
-c14 <- cces14 %>% as_survey_design(weights = weight)
-c16 <- cces16 %>% as_survey_design(weights = commonweight_vv)
-
-
-
-p1 <- c08 %>% 
-  filter(baprot2 ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2008")
-
-p2 <- c08 %>% 
+cc1 <- all_cc %>% 
   filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2008")
+  group_by(year) %>%  
+  filter(att >= 0) %>% 
+  summarise(mean = weighted.mean(att, w = weight, na.rm = TRUE),
+            sd = sd(att, na.rm = TRUE), 
+            n = n()) %>% 
+  mutate(se = sd/sqrt(n),
+         lower = mean - qt(1 - (0.05 /2),  n -1) * se,
+         upper = mean + qt(1 - (0.05 /2),  n -1) * se) %>% 
+  mutate(type = "Affiliation")
 
+cc2 <- all_cc %>% 
+  filter(protestant ==1 & bagain ==1 & race != 2) %>% 
+  group_by(year) %>%  
+  filter(att >= 0) %>% 
+  summarise(mean = weighted.mean(att, w = weight, na.rm = TRUE),
+            sd = sd(att, na.rm = TRUE), 
+            n = n()) %>% 
+  mutate(se = sd/sqrt(n),
+         lower = mean - qt(1 - (0.05 /2),  n -1) * se,
+         upper = mean + qt(1 - (0.05 /2),  n -1) * se) %>% 
+  mutate(type = "Self ID")
 
-p3 <- c10 %>% 
-  filter(baprot2 ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2010")
+ccc <- bind_rows(cc1, cc2)
 
-p4 <- c10 %>% 
-  filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2010")
+ccc$survey <- paste("CCES", ccc$year, sep = " ")
 
-
-p5 <- c12 %>% 
-  filter(baprot2 ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2012")
-
-p6 <- c12 %>% 
-  filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE))%>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2012")
-
-
-p7 <- c14 %>% 
-  filter(baprot2 ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2014")
-
-p8 <- c14 %>% 
-  filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2014")
-
-
-p9 <- c16 %>% 
-  filter(baprot2 ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Self ID") %>% 
-  mutate(survey = "CCES 2016")
-
-p10 <- c16 %>% 
-  filter(evangelical ==1) %>% 
-  summarise(mean = survey_mean(attend, vartype = "ci", level = .84, na.rm =TRUE)) %>% 
-  mutate(type = "Affiliation") %>% 
-  mutate(survey = "CCES 2016")
-
-pp <- bind_rows(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
 
 gss$att <- Recode(gss$attend, "8=6; 6:7=5; 4:5=4; 3=3; 2=2; 1=1; 0=0")
 
-gss <- gss %>% 
-  filter(year > 2007)  # Only 2008 waves and beyond
-  
-
-gs <- gss %>% as_survey_design(weights = wtssall)
-
-g1 <- gs %>% 
+gg1 <- gss %>% 
   filter(year >=2008) %>% 
   filter(evangelical ==1) %>% 
   group_by(year) %>% 
-  summarise(mean = survey_mean(att, vartype = "ci", level = .84, na.rm = TRUE)) %>% 
-  mutate(type = c("Affiliation"))
+  summarise(mean = weighted.mean(att, w = wtss, na.rm = TRUE),
+            sd = sd(att, na.rm = TRUE), 
+            n = n()) %>% 
+  mutate(se = sd/sqrt(n),
+         lower = mean - qt(1 - (0.05 /2),  n -1) * se,
+         upper = mean + qt(1 - (0.05 /2),  n -1) * se) %>% 
+  mutate(type = "Affiliation")
 
-g2 <- gs %>% 
+gg2 <- gss %>% 
   filter(year >=2008) %>%
-  filter(baprot2 ==1) %>% 
+  filter(reborn ==1 & relig ==1 & race != 2) %>% 
   group_by(year) %>% 
-  summarise(mean = survey_mean(att, vartype = "ci",  level = .84, na.rm = TRUE)) %>% 
+  summarise(mean = weighted.mean(att, w = wtss, na.rm = TRUE),
+            sd = sd(att, na.rm = TRUE), 
+            n = n()) %>% 
+  mutate(se = sd/sqrt(n),
+         lower = mean - qt(1 - (0.05 /2),  n -1) * se,
+         upper = mean + qt(1 - (0.05 /2),  n -1) * se) %>% 
   mutate(type = c("Self ID"))
 
-gg <- bind_rows(g1, g2)
+ggg <- bind_rows(gg1, gg2)
 
-gg$survey <- paste("GSS", gg$year, sep = " ")
+ggg$survey <- paste("GSS", ggg$year, sep = " ")
 
-total <- bind_rows(pp, gg) %>% select(-year)
+total2 <- bind_rows(ccc, ggg) %>% select(-year)
 
-ggplot(total, aes(x = mean, y = survey))  +
+ggplot(total2, aes(x = mean, y = survey))  +
   geom_point(shape=21, size =4, aes(fill = factor(type))) +  theme(legend.title=element_blank()) +
-  geom_errorbarh(aes(xmin = mean_low, xmax=mean_upp), height=.075, size = 1, show.legend = FALSE) +
+  geom_errorbarh(aes(xmin = lower, xmax=upper), height=.075, size = 1, show.legend = FALSE) +
   theme(legend.position = "bottom")  + scale_fill_brewer(palette = "Set2") + 
-  labs(x= "Year And Survey", y= "Self Reported Church Attendance", caption = "84% Confidence Intervals Displayed") +
+  labs(x= "Year And Survey", y= "Self Reported Church Attendance", caption = "95% Confidence Intervals Displayed") +
   ggtitle("Mean Church Attendance of Each Measurement")+ 
   theme(plot.title = element_text(hjust = 0.5))+
   theme(text=element_text(size=24, family="KerkisSans"))  +
